cd32724543e4e762f19c16307569fca6077baa64
alkhanuf <simoon.phv@gmail.com>
2026-04-29 00:19:20 +0800

    методичка

--- 5lab/Form1.cs
+++ 5lab/Form1.cs
@@ -5,26 +5,96 @@ namespace _5lab
     public partial class Form1 : Form
     {
 
-        MyRectangle myRect;
+        Player player;
+        Marker marker;
+        List<BaseObject> objects = new();
         public Form1()
         {
             InitializeComponent();
-            myRect = new MyRectangle(100, 100, 45);
+
+            player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
+            
+            
+            player.OnOverlap += (p, obj) =>
+            {
+                txtLog.Text = $"[{DateTime.Now:HH:mm:ss:ff}] Игрок пересекся с {obj}\n" + txtLog.Text;
+            };
+
+            player.OnMarkerOverlap += (m) =>
+            {
+                objects.Remove(m);
+                marker = null;
+            };
+
+            marker = new Marker(pbMain.Width / 2 + 50, pbMain.Height / 2 + 50, 0);
+
+            objects.Add(player);
+            objects.Add(marker);
+
+            objects.Add(new MyRectangle(50, 50, 0));
+            objects.Add(new MyRectangle(200, 200, 90));
         }
 
         private void pbMain_Paint(object sender, PaintEventArgs e)
         {
             var g = e.Graphics;
-
             g.Clear(Color.White);
 
-            // вытаскиваем матрицу преобразования Graphics
-            var matrix = g.Transform;
-            matrix.Translate(myRect.X, myRect.Y);
-            matrix.Rotate(myRect.Angle); // смещаем ее в пространстве
-            g.Transform = matrix; // устанавливаем новую матрицу
+            updatePlayer();
 
-            myRect.Render(g);
+            foreach (var obj in objects.ToList())
+            {
+                if (obj != player && player.Overlaps(obj, g))
+                {
+                    player.Overlap(obj);
+                }
+            }
+
+            foreach (var obj in objects)
+            {
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
+        }
+
+        private void updatePlayer()
+        {
+            if (marker != null)
+            {
+                float dx = marker.X - player.X;
+                float dy = marker.Y - player.Y;
+                float length = MathF.Sqrt(dx * dx + dy * dy);
+                dx /= length;
+                dy /= length;
+
+                player.vX += dx * 0.5f;
+                player.vY += dy * 0.5f;
+
+                player.Angle = 90 - MathF.Atan2(player.vX, player.vY) * 180 / MathF.PI;
+            }
+
+            player.vX += -player.vX * 0.1f;
+            player.vY += -player.vY * 0.1f;
+
+            player.X += player.vX;
+            player.Y += player.vY;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pbMain.Invalidate();
+        }
+
+        private void pbMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (marker == null)
+            {
+                marker = new Marker(0, 0, 0);
+                objects.Add(marker);
+            }
+
+            marker.X = e.X;
+            marker.Y = e.Y;
         }
     }
 }
--- 5lab/Objects/BaseObject.cs
+++ 5lab/Objects/BaseObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Drawing2D;
 using System.Text;
 
 namespace _5lab.Objects
@@ -10,6 +11,9 @@ namespace _5lab.Objects
         public float Y;
         public float Angle;
 
+        public Action<BaseObject, BaseObject> OnOverlap;
+
+
         public BaseObject(float x, float y, float angle)
         {
             X = x;
@@ -17,10 +21,45 @@ namespace _5lab.Objects
             Angle = angle;
         }
 
+        public Matrix GetTransform()
+        {
+            var matrix = new Matrix();
+            matrix.Translate(X, Y);
+            matrix.Rotate(Angle);
+            return matrix;
+        }
+
         public virtual void Render(Graphics g)
         {
-            // тут пусто
+            
         }
+
+        public virtual GraphicsPath GetGraphicsPath()
+        {
+            return new GraphicsPath();
+        }
+
+        public virtual bool Overlaps(BaseObject obj, Graphics g)
+        {
+            var path1 = this.GetGraphicsPath();
+            var path2 = obj.GetGraphicsPath();
+
+            path1.Transform(this.GetTransform());
+            path2.Transform(obj.GetTransform());
+
+            var region = new Region(path1);
+            region.Intersect(path2);
+            return !region.IsEmpty(g); 
+        }
+
+        public virtual void Overlap(BaseObject obj)
+        {
+            if (this.OnOverlap != null)
+            {
+                this.OnOverlap(this, obj);
+            }
+        }
+
     }
 
 }
